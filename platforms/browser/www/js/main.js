664a5a6adaf31838b86ce095792d08546f9c1bb7
jix2015 <jdoming@alumnos.ubiobio.cl>
2017-06-10 01:07:03 -0400

    Commit 7
    Ya se pueden subir imagenes al servidor y su info a la BD.

--- platforms/browser/www/js/main.js
+++ platforms/browser/www/js/main.js
@@ -1,3 +1,5 @@
+
+
 // Initialize your app
 var myApp = new Framework7();
 
@@ -10,31 +12,32 @@ var mainView = myApp.addView('.view-main', {
     dynamicNavbar: true
 });
 
+
 document.addEventListener('deviceready', function(){
     $('#name').html('<b>' + localStorage.getItem('nombre') + '</b>');
     $('#mail').html('<b>' + localStorage.getItem('mail') + '</b>');
-    $('#cam').bind('click', cam);
+    $('#cam').bind('click', capturarFoto);
+    $('#video').bind('click', capturarVideo);
+    $('#sacarFoto').bind('click', tomarFoto);
+    $('#subir').bind('click', subirFoto);
     $('#ver').bind('click', ver);
-    $('#video').bind('click',video);
+    $('#grabarVideo').bind('click',video);
+     $('#subVideo').bind('click',subirVideo);
     $('#cerrar').bind('click',cerrar);
+    $('#volver').bind('click',volveratras);
     $('#vervideo').bind('click',vervideo);
 }, false);
 
-function cam(){
-    navigator.camera.getPicture(function(photo){
-        myApp.prompt('Agregue una Descripción a la Imagen','REMEMBER', function (value) {
-        $('#img_cam').attr('src',photo);
-        myApp.popup('.popup-cam');
-            });
-    }, function(error){
-        myApp.alert('Error al tomar la fotografía','REMEMBER')
-    }, {
-        quality:100,
-        correctOrientation:true,
-        saveToPhotoAlbum:true,
-        cameraDirection: 1
-    });
+
+function capturarFoto(){
+    window.location = "foto.html";
 }
+
+function capturarVideo(){
+   window.location = "video.html"; 
+}
+
+
 
 function ver(){
     window.location = "main2.html";
@@ -59,8 +62,52 @@ function video(){
     //myApp.prompt('Agregue una Descripción al video','REMEMBER', function (value) {});
 }
 
+function subirVideo() {
+    if($('#videolocal').attr('src') != ''){
+        var imageURI = $('#videolocal').attr('src');
+        var options = new FileUploadOptions();
+        options.fileKey="video";
+        options.fileName=imageURI.substr(imageURI.lastIndexOf('/')+1);
+        
+        var params = new Object();
+        params.descripcion = document.getElementById("descripcion").value;
+        options.params = params;
+        options.chunkedMode = false;
+        
+        myApp.showPreloader('Subiendo...');
+        
+        var ft = new FileTransfer();
+        ft.upload(imageURI, "http://colvin.chillan.ubiobio.cl:8070/jdoming/uploadVideo.php", win2, fail2, options);
+        localStorage.setItem('ruta', imageURI.substr(imageURI.lastIndexOf('/') + 1));
+        localStorage.setItem('descripcion',params.descripcion);
+    }else{
+        myApp.alert('No hay foto para subir','REMEMBER');
+    }
+}
+
+
+function win2(r) {
+    console.log(r.response);
+    myApp.hidePreloader();
+    myApp.alert('Imagen Subida exitosamente:'+ r.response,'REMEMBER');
+    guardarFoto();
+    
+    
+}
+
+function fail2(error) {
+    myApp.hidePreloader();
+    myApp.alert('Error al subir la imagen','REMEMBER');
+}
+
+function error2(){
+    console.log("ERROR");
+}
+
+
+
 function cerrar(){
-    $$('.confirm-ok').on('click', function () {
+    $('.confirm-ok').on('click', function () {
     myApp.confirm('Está seguro de cerrar sesión','REMEMBER', function () {
        window.location = "index.html";
     });
@@ -69,6 +116,97 @@ function cerrar(){
     
 }
 
+function volveratras(){
+      $('.confirm-ok').on('click', function () {
+    myApp.confirm('Está seguro regresar al menú principal','REMEMBER', function () {
+       window.location = "main.html";
+    });
+         
+});
+}
+
 function vervideo(){
     window.location = "mainvideo.html";
 }
+
+
+
+
+function tomarFoto() {
+    // Retrieve image file location from specified source
+    navigator.camera.getPicture(function(imageURI){
+            $('#fotolocal').attr('src',imageURI);
+        }, function(message) {
+            myApp.alert('Error al tomar la fotografía','REMEMBER');
+        },{
+            quality: 40,
+            destinationType: navigator.camera.DestinationType.FILE_URI,
+            sourceType: navigator.camera.PictureSourceType.CAMERA
+        }
+    );
+
+}
+
+
+
+function subirFoto() {
+    if($('#fotolocal').attr('src') != ''){
+        var imageURI = $('#fotolocal').attr('src');
+        var options = new FileUploadOptions();
+        options.fileKey="imagen";
+        options.fileName=imageURI.substr(imageURI.lastIndexOf('/')+1);
+        options.mimeType="image/jpeg";
+        var params = new Object();
+        params.descripcion = document.getElementById("descripcion").value;
+        options.params = params;
+        options.chunkedMode = false;
+        
+        myApp.showPreloader('Subiendo...');
+        
+        var ft = new FileTransfer();
+        ft.upload(imageURI, "http://colvin.chillan.ubiobio.cl:8070/jdoming/uploadimagen.php", win, fail, options);
+        localStorage.setItem('ruta', imageURI.substr(imageURI.lastIndexOf('/') + 1));
+        localStorage.setItem('descripcion',params.descripcion);
+    }else{
+        myApp.alert('No hay foto para subir','REMEMBER');
+    }
+}
+
+function win(r) {
+    console.log(r.response);
+    myApp.hidePreloader();
+    myApp.alert('Imagen Subida exitosamente:'+ r.response,'REMEMBER');
+    guardarFoto();
+    
+    
+}
+
+function fail(error) {
+    myApp.hidePreloader();
+    myApp.alert('Error al subir la imagen','REMEMBER');
+}
+
+function error(){
+    console.log("ERROR");
+}
+
+function guardarFoto(){
+    var descripcion = localStorage.getItem('descripcion');
+    var Email = localStorage.getItem('Email');
+    var ruta = localStorage.getItem('ruta');
+      $.ajax({
+          dataType: 'json',
+          type: 'POST',
+          data: {
+              descripcion: descripcion,
+              Email: Email,
+              ruta:ruta
+          },
+          url: 'http://colvin.chillan.ubiobio.cl:8070/jdoming/guardarFoto.php'
+        
+      });
+}
+
+
+
+
